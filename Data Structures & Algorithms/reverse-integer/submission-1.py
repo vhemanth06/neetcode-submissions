fb1323a27ce77072b9e234855797fd050906f912
Vuppula Hemanth Reddy <vhemanthreddy06@gmail.com>
2026-08-26 16:12:45 +0530

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        intmax=2**31-1
+        if x==0:
+            return 0
+        m=int(x/abs(x))
+        x=abs(x)
+        res=0
+        while x!=0:
+            a=x%10
+            x=x//10
+            res=res*10+a
+            if res>intmax:
+                return 0
+        return res*m
+            
