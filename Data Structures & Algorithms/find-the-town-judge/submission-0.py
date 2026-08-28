ea6c307aead9aa4136a735cfb2b02e11d8334c62
Vuppula Hemanth Reddy <vhemanthreddy06@gmail.com>
2026-08-28 17:59:19 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,12 @@
+from collections import defaultdict,deque
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        source=[0]*n
+        target=[0]*n
+        for x,y in trust:
+            source[x-1]+=1
+            target[y-1]+=1
+        for i in range(n):
+            if source[i]==0 and target[i]==n-1:
+                return i+1
+        return -1
