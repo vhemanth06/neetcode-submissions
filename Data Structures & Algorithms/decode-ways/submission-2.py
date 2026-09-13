f22a94874e277e01538ca017a6c77367da0aac08
Vuppula Hemanth Reddy <vhemanthreddy06@gmail.com>
2026-09-14 00:41:05 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo={len(s):1}
+        n=len(s)
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            if s[i]=='0':
+                return 0
+            res=dp(i+1)
+            if i+1<n:
+                if s[i]=='1' or (s[i]=='2' and s[i+1] in "0123456"):
+                    res+=dp(i+2)
+            memo[i]=res
+            return memo[i]
+        return dp(0)
